c07125daa21fc38e98fd7732d2aa9eac77256a11
carlosbkm <carlos.garcia.mejias@gmail.com>
2017-04-13 18:10:03 +0200

    Refactored data extraction and augmentation

--- clone.py
+++ clone.py
@@ -23,40 +23,49 @@ def get_path_separator ():
   print('file separator :', path_separator)
   return path_separator
 
+def get_augmented_data (images, angles):
+  augmented_images, augmented_angles = [], []
+  for image, angle in zip(images, angles):
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_angles.append(angle*-1.0)
+  return augmented_images, augmented_angles
+
+def get_data_from_csv_rows(rows):
+  images, angles = [], []
+  i = 0
+  for row in rows:
+    steering_center = float(row[3])
+
+    # create adjusted steering angles for the side camera images
+    correction = 0.2 # this is a parameter to tune
+    steering_left = steering_center + correction
+    steering_right = steering_center - correction
+
+    image_center = cv2.imread(extract_actual_path(row[0], file_separator))
+    image_left = cv2.imread(extract_actual_path(row[1], file_separator))
+    image_right = cv2.imread(extract_actual_path(row[2], file_separator))
+
+    images.append(image_center)
+    images.append(image_left)
+    images.append(image_right)
+
+    angles.append(steering_center)
+    angles.append(steering_left)
+    angles.append(steering_right)
+
+  return images, angles
+
 lines = get_lines_from_csv('driving_log.csv')
 file_separator = get_path_separator()
+images, angles = get_data_from_csv_rows(lines)
+augmented_images, augmented_angles = get_augmented_data(images, angles)
+images.extend(augmented_images)
+angles.extend(augmented_angles)
 
-images = []
-measurements = []
-for line in lines:
-  steering_center = float(line[3])
+from sklearn.utils import shuffle
 
-  # create adjusted steering measurements for the side camera images
-  correction = 0.2 # this is a parameter to tune
-  steering_left = steering_center + correction
-  steering_right = steering_center - correction
-
-  image_center = cv2.imread(extract_actual_path(line[0], file_separator))
-  image_left = cv2.imread(extract_actual_path(line[1], file_separator))
-  image_right = cv2.imread(extract_actual_path(line[2], file_separator))
-
-  images.append(image_center)
-  images.append(image_left)
-  images.append(image_right)
-
-  measurements.append(steering_center)
-  measurements.append(steering_left)
-  measurements.append(steering_right)
-
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-  augmented_images.append(image)
-  augmented_measurements.append(measurement)
-  augmented_images.append(cv2.flip(image, 1))
-  augmented_measurements.append(measurement*-1.0)
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+X_train = np.array(shuffle(images))
+y_train = np.array(shuffle(angles))
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
