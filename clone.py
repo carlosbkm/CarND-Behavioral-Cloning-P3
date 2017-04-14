4deb0c9e0ee328946376e37866f1437818c409f9
Carlos Garcia <carlos.garcia.mejias@gmail.com>
2017-04-14 18:18:47 +0200

    Reduced steps per epoch

--- clone.py
+++ clone.py
@@ -132,8 +132,8 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, steps_per_epoch = 10036, validation_data=validation_generator,
-                    validation_steps=2510, epochs=3)
+model.fit_generator(train_generator, steps_per_epoch = len(train_samples)//16, validation_data=validation_generator,
+                    validation_steps=len(validation_samples)//16, epochs=4)
 
 #model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
